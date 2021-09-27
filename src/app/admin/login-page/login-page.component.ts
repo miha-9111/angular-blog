9d1806e1dc153937c80743941ad27e7e274ec677
miha-9111 <miha-9111@yandex.ru>
2021-09-27 05:07:56 +0300

    store firebase token v.1

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -11,7 +11,8 @@ import {Router} from "@angular/router";
 })
 export class LoginPageComponent implements OnInit {
 
-  form!: FormGroup
+  form!: FormGroup;
+  submitted = false;
 
   constructor(private auth: AuthService,
               private router: Router) {
@@ -36,6 +37,8 @@ export class LoginPageComponent implements OnInit {
       return
     }
 
+    this.submitted = true;
+
     const user: User = {
       email: this.form.value.email,
       password: this.form.value.password
@@ -44,6 +47,7 @@ export class LoginPageComponent implements OnInit {
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
     })
   }
 }
--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -11,10 +11,16 @@ export class AuthService {
   }
 
   get token(): string {
-    return '';
+    const expDate = new Date(localStorage.getItem('fb-token-exp'));
+    if (new Date() > expDate) {
+      this.logout();
+      return null;
+    }
+    return localStorage.getItem('fb-token');
   }
 
   login(user: User): Observable<any> {
+    user.returnSecureToken = true;
     return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
       .pipe(
         tap(this.setToken)
@@ -22,14 +28,20 @@ export class AuthService {
   }
 
   logout() {
-
+    this.setToken(null);
   }
 
   isAuthenticated(): boolean {
     return !!this.token;
   }
 
-  private setToken(response: FbAuthResponse) {
-    console.log(response);
+  private setToken(response: FbAuthResponse | null) {
+    if (response) {
+      const expDate = new Date(new Date().getTime() + +response.expiresIn * 1000);
+      localStorage.setItem('fb-token', response.idToken);
+      localStorage.setItem('fb-token-exp', expDate.toString());
+    } else {
+      localStorage.clear();
+    }
   }
 }

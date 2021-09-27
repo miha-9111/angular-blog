0954ad3fedeceeca7ff687d0941a8bd6972e3249
miha-9111 <miha-9111@yandex.ru>
2021-09-27 04:09:19 +0300

    added firebase/token v.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyCcDbvwJX6bMmVxgeybJl0UpkJXM6x4G6Q'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import {Environment} from "./interface";
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyCcDbvwJX6bMmVxgeybJl0UpkJXM6x4G6Q'
 };
 
 /*

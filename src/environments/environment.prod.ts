75f175197213b39bb3920d651707a134226411ee
miha-9111 <miha-9111@yandex.ru>
2021-09-29 00:32:35 +0300

    added posts.service -> create post v.1

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,6 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: true,
-  apiKey: 'AIzaSyCcDbvwJX6bMmVxgeybJl0UpkJXM6x4G6Q'
+  apiKey: 'AIzaSyCcDbvwJX6bMmVxgeybJl0UpkJXM6x4G6Q',
+  fbDbUrl: 'https://angular-blog-56cd9-default-rtdb.firebaseio.com'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,8 @@ import {Environment} from "./interface";
 
 export const environment: Environment = {
   production: false,
-  apiKey: 'AIzaSyCcDbvwJX6bMmVxgeybJl0UpkJXM6x4G6Q'
+  apiKey: 'AIzaSyCcDbvwJX6bMmVxgeybJl0UpkJXM6x4G6Q',
+  fbDbUrl: 'https://angular-blog-56cd9-default-rtdb.firebaseio.com'
 };
 
 /*

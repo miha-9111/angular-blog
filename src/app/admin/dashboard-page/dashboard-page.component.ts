1733bac1551d9affa2c1c240c7645ccbd42ca85a
miha-9111 <miha-9111@yandex.ru>
2021-09-29 04:20:33 +0300

    dashboard-page -> added SearchPipe v.1

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -12,6 +12,7 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
 
   posts: Post[] = [];
   pSub: Subscription;
+  searchStr: string = '';
 
   constructor(private postsService: PostsService) { }
 
